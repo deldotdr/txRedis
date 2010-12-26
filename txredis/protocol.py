ca3e1b6c7612df4eee71e841d86d8b6c7caea1c6
John Khvatov <ivaxer@gmail.com>
2010-12-26 20:12:52 +0300

    Fixed bug in hget

--- txredis/protocol.py
+++ txredis/protocol.py
@@ -1023,6 +1023,8 @@ class Redis(RedisBase):
         def post_process(values):
             if not values:
                 return values
+            if isinstance(field, basestring):
+                return {field: values}
             return dict(izip(field, values))
 
         return self.getResponse().addCallback(post_process)
