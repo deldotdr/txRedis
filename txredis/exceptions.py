5346d49a57bd7cbc90d8325658807a7cb014a7fb
Duncan McGreggor <duncan@dreamhost.com>
2011-12-21 09:38:19 -0800

    * Created a new module and moved the exceptions into there (this included   updating all the references to those exceptions). * Added a todo comment about fixing a bad variable reference in a future   branch.

---
+++ txredis/exceptions.py
@@ -0,0 +1,22 @@
+class RedisError(Exception):
+    pass
+
+
+class ConnectionError(RedisError):
+    pass
+
+
+class ResponseError(RedisError):
+    pass
+
+
+class InvalidResponse(RedisError):
+    pass
+
+
+class InvalidData(RedisError):
+    pass
+
+
+class InvalidCommand(RedisError):
+    pass
