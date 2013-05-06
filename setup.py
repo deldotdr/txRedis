581bf0aca1c0fc4eb75eb610d9939ee496968a98
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-05-06 13:23:20 -0700

    Changed setup.py to use standard usage.

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-#!/usr/bin/env python
-sdict = {
-    'name' : 'txredis',
-    'version' : '2.2',
-    'packages' : ['txredis'],
-    'description' : 'Python/Twisted client for Redis key-value store',
-    'author' : 'Dorian Raymer',
-    'author_email' : 'deldotdr@gmail.com',
-    'maintainer' : 'Reza Lotun',
-    'maintainer_email' : 'rlotun@gmail.com',
-    'keywords': ['Redis', 'key-value store', 'Twisted'],
-    'classifiers' : [
+from setuptools import setup
+
+
+setup(
+    name='txredis',
+    version='2.2',
+    packages=['txredis'],
+    description='Python/Twisted client for Redis key-value store',
+    author='Dorian Raymer',
+    author_email='deldotdr@gmail.com',
+    maintainer='Reza Lotun',
+    maintainer_email='rlotun@gmail.com',
+    keywords=['Redis', 'key-value store', 'Twisted'],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Framework :: Twisted',
-        ],
-}
-
-from setuptools import setup
-setup(**sdict)
+        'Framework :: Twisted'])
