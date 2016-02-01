4294a81aa83868b32faf4789923b409b09a4ed41
Grigorii Eremeev <budulianin@gmail.com>
2016-02-01 13:40:23 +0000

    Issue-54: moved auth method to base class

--- txredis/client.py
+++ txredis/client.py
@@ -978,24 +978,6 @@ class RedisClient(RedisBase):
         self._send(*stmt)
         return self.getResponse()
 
-    def auth(self, passwd):
-        """
-        Request for authentication in a password protected Redis server. Redis
-        can be instructed to require a password before allowing clients to
-        execute commands. This is done using the requirepass directive in the
-        configuration file.  If password matches the password in the
-        configuration file, the server replies with the OK status code and
-        starts accepting commands. Otherwise, an error is returned and the
-        clients needs to try a new password.
-
-        Note: because of the high performance nature of Redis, it is possible
-        to try a lot of passwords in parallel in very short time, so make sure
-        to generate a strong and very long password so that this attack is
-        infeasible.
-        """
-        self._send('AUTH', passwd)
-        return self.getResponse()
-
     def quit(self):
         """
         Ask the server to close the connection. The connection is closed as
