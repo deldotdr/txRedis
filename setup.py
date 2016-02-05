ec2aa9aca01fe55eaa843e937f9d0f977bfcb881
Dorian Raymer <deldotdr@gmail.com>
2016-02-04 16:27:12 -0800

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 sdict = {
     'name': 'txredis',
-    'version': '2.3',
+    'version': '2.4',
     'packages': ['txredis'],
     'description': 'Python/Twisted client for Redis key-value store',
     'author': 'Dorian Raymer',
